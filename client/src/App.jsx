640b0a4de3177cc19918e26298ba2b1c3e6e9242
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-02 04:33:43 +0100

    CODE: Installed client prerequisites including tailwind and firebase

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-2xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
