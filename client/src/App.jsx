07d17591d29c046d807bffd35f25096dff908ece
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-06 19:21:50 +0100

    CODE: Created basic pages, prepared App layout, added header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Layout from './pages/Layout'
+import Home from './pages/Home'
 
 function App() {
 
-
   return (
-    <>
-      <h1 className="text-2xl text-red-500 font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout />}/>
+        <Route index element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
