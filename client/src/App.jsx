5cf7813e6afaaf440c4c463486a4945e94c27ba4
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-06 20:33:05 +0100

    CODE: Adjusted Header, added Menu dropdown

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 
 import Layout from './pages/Layout'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
 
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Layout />}/>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </div>
   )
