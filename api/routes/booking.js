8a26b6890b3fef482dc3f735ae2c251f78409e41
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-15 00:00:23 +0100

    UPDATE: Defined all booking routes

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -1,12 +1,18 @@
 import { verifyManagerOrAdmin } from "../middlewares/verifyManagerOrAdmin"
+import router from "./property"
 
 //Create a booking
 router.route('/create')
     .post(createBooking)
 
-//Get all bookings by user id
+//Get all bookings of a specific user
+router.route('user/all')
+    .get(getUserBookings)
+
+//Get a booking by id
 router.route('user/:id')
-    .get(getUserBooking)
+    .get(getUserBookingsById)
+
 
 // Manager or Admins Below
 
@@ -18,3 +24,10 @@ router.route('/all')
 router.route('/:id')
     .get(verifyManagerOrAdmin, getBooking)
 
+//Update a booking by id as admin or manager
+router.route('/:id')
+    .patch(verifyManagerOrAdmin, updateBooking)
+
+//Delete a booking by id as admin or manager
+router.route('/:id')
+    .delete(verifyManagerOrAdmin, deleteBooking)
