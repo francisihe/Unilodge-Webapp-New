db883c9cd1971fc97b3bfef1e16257dfad37a7ea
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-17 22:52:10 +0100

    CODE: Created a getUserByIdentifier middleware to enable User Route actions by admins use either User ObjectId or User emails

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { verifyUser } from '../middlewares/verifyUser.js';
 import { verifyAdmin } from '../middlewares/verifyAdmin.js';
 import { deleteUser, getAllUsers, getUserById, updateUser, updateUserAsAdmin } from '../controllers/userController.js';
+import { getUserByIdentifier } from '../middlewares/getUserByIdentifier.js';
 const router = express.Router();
 
 // Update User
@@ -10,20 +11,25 @@ router.route('/:userId')
 
 // Admin Routes Below
 
+/* For admin routes, userId can handle either the objectId or Email.
+This is to enable the FE easily find users using their email address too
+This is done using the getUserByIdentifier middleware
+*/
+
 // Get All Users
 router.route('/all')
     .get(verifyAdmin, getAllUsers)
 
 // Get User By Id
 router.route('/:userId')
-    .get(verifyAdmin, getUserById)
+    .get(verifyAdmin, getUserByIdentifier, getUserById)
 
 // Update User As Admin
 router.route('/:userId/update')
-    .patch(verifyAdmin, updateUserAsAdmin)
+    .patch(verifyAdmin, getUserByIdentifier, updateUserAsAdmin)
 
 // Delete User 
 router.route('/:userId')
-    .delete(verifyAdmin, deleteUser)
+    .delete(verifyAdmin, getUserByIdentifier, deleteUser)
 
 export default router;
