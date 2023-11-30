4269a4a9be72d8636834c92825fc561baf9d73e9
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-11-30 12:17:54 +0100

    UPDATE: Adjusted search forms to fit properly on mobile

--- client/src/components/forms/PropertyListFilterForm.jsx
+++ client/src/components/forms/PropertyListFilterForm.jsx
@@ -28,66 +28,69 @@ const PropertyListFilterForm = ({
         setPropertyModel('');
         setPropertyStatus('');
         setPropertyCategory('');
-        onClear(); 
+        onClear();
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit} className="border-2 px-4 py-2 rounded-xl">
                 <div className="md:flex md:gap-2 md:items-center">
-                    <div className="flex gap-1 justify-between md:pr-10 md:w-4/5">
+                    <div className="flex gap-3 md:gap-0 justify-evenly md:justify-between md:w-4/5">
+                        <div className="md:flex items-center justify-evenly w-2/4">
+                            <div className="grid">
+                                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Type</label>
+                                <select
+                                    value={propertyType}
+                                    onChange={(e) => setPropertyType(e.target.value)}
+                                    className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                                >
+                                    <option value=''>Select</option>
+                                    <option value="house">House</option>
+                                    <option value="land">Land</option>
+                                </select>
+                            </div>
 
-                        <div className="grid">
-                            <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Type</label>
-                            <select
-                                value={propertyType}
-                                onChange={(e) => setPropertyType(e.target.value)}
-                                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-                            >
-                                <option value=''>Select</option>
-                                <option value="house">House</option>
-                                <option value="land">Land</option>
-                            </select>
+                            <div className="grid">
+                                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Model</label>
+                                <select
+                                    value={propertyModel}
+                                    onChange={(e) => setPropertyModel(e.target.value)}
+                                    className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                                >
+                                    <option value=''>Select</option>
+                                    <option value="house">House</option>
+                                    <option value="hostel">Hostel</option>
+                                    <option value="land">Land</option>
+                                </select>
+                            </div>
                         </div>
 
-                        <div className="grid">
-                            <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Model</label>
-                            <select
-                                value={propertyModel}
-                                onChange={(e) => setPropertyModel(e.target.value)}
-                                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-                            >
-                                <option value=''>Select</option>
-                                <option value="house">House</option>
-                                <option value="hostel">Hostel</option>
-                                <option value="land">Land</option>
-                            </select>
-                        </div>
+                        <div className="md:flex items-center justify-evenly w-2/4">
+                            <div className="grid">
+                                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Status</label>
+                                <select
+                                    value={propertyStatus}
+                                    onChange={(e) => setPropertyStatus(e.target.value)}
+                                    className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                                >
+                                    <option value=''>Select</option>
+                                    <option value="available">Available</option>
+                                    <option value="taken">Taken</option>
+                                </select>
+                            </div>
 
-                        <div className="grid">
-                            <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Status</label>
-                            <select
-                                value={propertyStatus}
-                                onChange={(e) => setPropertyStatus(e.target.value)}
-                                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-                            >
-                                <option value=''>Select</option>
-                                <option value="available">Available</option>
-                                <option value="taken">Taken</option>
-                            </select>
-                        </div>
-
-                        <div className="grid">
-                            <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Category</label>
-                            <select
-                                value={propertyCategory}
-                                onChange={(e) => setPropertyCategory(e.target.value)}
-                                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-                            >
-                                <option value=''>Select</option>
-                                <option value="rent">Rent</option>
-                                <option value="sale">Sale</option>
-                            </select>
+                            <div className="grid">
+                                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Category</label>
+                                <select
+                                    value={propertyCategory}
+                                    onChange={(e) => setPropertyCategory(e.target.value)}
+                                    className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                                >
+                                    <option value=''>Select</option>
+                                    <option value="rent">Rent</option>
+                                    <option value="sale">Sale</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
 
--- client/src/components/forms/PropertySearchAndFilter.jsx
+++ client/src/components/forms/PropertySearchAndFilter.jsx
@@ -45,7 +45,7 @@ const PropertySearchAndFilter = ({
           <div className="flex items-center gap-1">
             <input
               type="text"
-              value={ searchTerm }
+              value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="Enter search term"
               className="w-3/4"
@@ -61,59 +61,62 @@ const PropertySearchAndFilter = ({
         </div>
 
         <div className="md:flex md:items-center lg:w-3/5">
-          <div className="flex gap-2 justify-between md:pr-10 md:w-4/5">
+          <div className="flex gap-2 md:gap-4 justify-between md:pr-10 md:w-4/5">
+            <div className="md:flex md:gap-2 items-center justify-evenly w-2/4">
+              <div className="grid">
+                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Type</label>
+                <select
+                  value={propertyType}
+                  onChange={(e) => setPropertyType(e.target.value)}
+                  className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                >
+                  <option value=''>Select</option>
+                  <option value="house">House</option>
+                  <option value="land">Land</option>
+                </select>
+              </div>
 
-            <div className="grid">
-              <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Type</label>
-              <select
-                value={propertyType}
-                onChange={(e) => setPropertyType(e.target.value)}
-                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-              >
-                <option value=''>Select</option>
-                <option value="house">House</option>
-                <option value="land">Land</option>
-              </select>
+              <div className="grid">
+                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Model</label>
+                <select
+                  value={propertyModel}
+                  onChange={(e) => setPropertyModel(e.target.value)}
+                  className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                >
+                  <option value=''>Select</option>
+                  <option value="house">House</option>
+                  <option value="hostel">Hostel</option>
+                  <option value="land">Land</option>
+                </select>
+              </div>
             </div>
 
-            <div className="grid">
-              <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Model</label>
-              <select
-                value={propertyModel}
-                onChange={(e) => setPropertyModel(e.target.value)}
-                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-              >
-                <option value=''>Select</option>
-                <option value="house">House</option>
-                <option value="hostel">Hostel</option>
-                <option value="land">Land</option>
-              </select>
-            </div>
+            <div className="md:flex md:gap-2 items-center justify-evenly w-2/4">
+              <div className="grid">
+                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Status</label>
+                <select
+                  value={propertyStatus}
+                  onChange={(e) => setPropertyStatus(e.target.value)}
+                  className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                >
+                  <option value=''>Select</option>
+                  <option value="available">Available</option>
+                  <option value="taken">Taken</option>
+                </select>
+              </div>
 
-            <div className="grid">
-              <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Status</label>
-              <select
-                value={propertyStatus}
-                onChange={(e) => setPropertyStatus(e.target.value)}
-                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-              >
-                <option value=''>Select</option>
-                <option value="available">Available</option>
-                <option value="taken">Taken</option>
-              </select>
-            </div>
-
-            <div className="grid">
-              <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Category</label>
-              <select
-                value={propertyCategory}
-                onChange={(e) => setPropertyCategory(e.target.value)}
-                className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
-              >
-                <option value=''>Select</option>
-                <option value="rent">Rent</option>
-                <option value="sale">Sale</option>
-              </select>
+              <div className="grid">
+                <label className="text-xs text-orange-500 font-medium whitespace-nowrap">Property Category</label>
+                <select
+                  value={propertyCategory}
+                  onChange={(e) => setPropertyCategory(e.target.value)}
+                  className="text-sm w-full border my-1 py-1 px-1 rounded-2xl md:rounded-sm bg-inherit"
+                >
+                  <option value=''>Select</option>
+                  <option value="rent">Rent</option>
+                  <option value="sale">Sale</option>
+                </select>
+              </div>
             </div>
           </div>
 
