2c2b9f5bd7650f37fbf480ed1a5621515a799a96
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-12-02 01:23:40 +0100

    UPDATE: Enabled Editing and Deleting Bookings on Summary page

--- client/src/pages/admin/DashboardSummary.jsx
+++ client/src/pages/admin/DashboardSummary.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { SiIcon } from "react-icons/si";
 import BookingCardMaxi from "../../components/UIelements/BookingCardMaxi";
 import Pagination from "../../components/UIelements/Pagination";
-
+import BookingEditForm from "../../components/forms/BookingEditForm";
+import Modal from "react-modal";
 
 const DashboardSummary = () => {
   const [dashboardSummaryOpen, setDashboardSummaryOpen] = useState(true);
@@ -15,6 +16,11 @@ const DashboardSummary = () => {
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalProperties, setTotalProperties] = useState(0);
   const [totalFeaturedProperties, setTotalFeaturedProperties] = useState(0);
+
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [updateCount, setUpdateCount] = useState(0); // Just to rerender the useEffect when Update Form is updated
 
   // Pagination States
   const [currentPage, setCurrentPage] = useState(1);
@@ -60,6 +66,55 @@ const DashboardSummary = () => {
     return `${weekday}, ${day} ${month}, ${year}`;
   }
   const todaysDate = getTodayDateWithWeekday();
+
+  const formatDate = (dateString) => {
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+  };
+
+  const openEditModal = (booking) => {
+    setSelectedBooking(booking);
+    setIsEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setSelectedBooking(null);
+    setIsEditModalOpen(false);
+  };
+
+  const openDeleteModal = (booking) => {
+    setSelectedBooking(booking);
+    setIsDeleteModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  // Update the bookings list when the update form is submitted
+  const handleUpdateBookings = () => {
+    // Increment the ref value to trigger useEffect
+    setUpdateCount(updateCount + 1);
+  };
+
+  const handleDeleteBooking = async () => {
+    const res = await fetch(`/api/v1/bookings/${selectedBooking._id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    handleUpdateBookings(); // To refresh the bookings list
+    closeDeleteModal(); // To close the modal
+
+    if (!res.ok) {
+      console.log(data.message)
+    }
+
+    alert(`Booking has been deleted`)
+    console.log(`Booking ${selectedBooking?._id} with inspection date ${formatDate(selectedBooking?.inspectionDate)} has been deleted`)
+  };
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -143,10 +198,52 @@ const DashboardSummary = () => {
                     <BookingCardMaxi
                       key={booking._id}
                       booking={booking}
+                      openEditModal={() => openEditModal(booking)}
+                      openDeleteModal={() => openDeleteModal(booking)}
                     />
                   ))
                 }
               </div>
+
+              {/* Edit Booking Modal */}
+              <Modal
+                isOpen={isEditModalOpen}
+                onRequestClose={closeEditModal}
+                contentLabel="Update Booking Modal"
+                ariaHideApp={false}
+                className="fixed top-1/2 left-1/2 overflow-y-scroll transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-md md:max-w-md w-[85%] md:w-full"
+                overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-20 flex items-center justify-center"
+              >
+                <BookingEditForm
+                  selectedBooking={selectedBooking}
+                  onClose={closeEditModal}
+                  closeModal={() => closeEditModal(selectedBooking)}
+                  openDeleteModal={() => openDeleteModal(selectedBooking)}
+                  updateBookings={handleUpdateBookings}
+                />
+              </Modal>
+
+              {/* Delete Booking Modal */}
+              <Modal
+                isOpen={isDeleteModalOpen}
+                onRequestClose={closeDeleteModal}
+                contentLabel="Delete Booking Modal"
+                ariaHideApp={false}
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-md max-w-md w-full"
+                overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center"
+              >
+
+                <div className="flex flex-col space-y-4">
+                  <p className="mx-auto text-center">
+                    Are you sure you want to delete this booking? <br />< br />
+                    Booking ID {selectedBooking?._id} with inspection date {formatDate(selectedBooking?.inspectionDate)}
+                  </p>
+                  <div className="flex gap-2 mx-auto">
+                    <button onClick={handleDeleteBooking} className="bg-red-500 rounded-lg px-2 py-1">Yes, proceed</button>
+                    <button onClick={closeDeleteModal} className="bg-green-500 rounded-lg px-2 py-1">No, return</button>
+                  </div>
+                </div>
+              </Modal>
 
               <div className="flex justify-end py-2">
                 {loading
